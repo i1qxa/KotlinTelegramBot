9cb63b75634bcdceccc929ab53fdea6dcbdef44b
Иван <cvb@sdf>
2024-10-01 22:30:51 +0300

    KTB-03 Добавил чтение из файла в список Word

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,33 @@ const val FILE_NAME = "words.txt"
 
 fun main() {
 
-    File(FILE_NAME).readLines().forEach {
-        println(it)
+    val dictionary = mutableListOf<Word>()
+    File(FILE_NAME).readLines().forEach { wordItem ->
+        wordItem.split("|").apply {
+            if (validateDictionaryString(this)) {
+                val numberLearnedWord = try {
+                    this[2].toInt()
+                } catch (e: Exception) {
+                    0
+                }
+                dictionary.add(Word(this[0], this[1], numberLearnedWord))
+            }
+        }
     }
+    println(dictionary.joinToString(separator = "\n"))
 
 }
+
+fun validateDictionaryString(listWord: List<String>): Boolean {
+    return try {
+        listWord[0].isNotEmpty() && listWord[1].isNotEmpty()
+    } catch (e: Exception) {
+        false
+    }
+}
+
+data class Word(
+    val original: String,
+    val translated: String,
+    val numberLearnedWord: Int,
+)
