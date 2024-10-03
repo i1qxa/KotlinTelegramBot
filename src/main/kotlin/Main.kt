767caa6f78879321d27f34f8a55d3ac25d64969b
Иван <cvb@sdf>
2024-10-03 20:57:39 +0300

    KTB-03 Переименовал поле класса Word, упростил добавление новых слов в словарь

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,31 +8,19 @@ fun main() {
 
     val dictionary = mutableListOf<Word>()
     File(FILE_NAME).readLines().forEach { wordItem ->
-        wordItem.split("|").apply {
-            if (validateDictionaryString(this)) {
-                val numberLearnedWord = try {
-                    this[2].toInt()
-                } catch (e: Exception) {
-                    0
-                }
-                dictionary.add(Word(this[0], this[1], numberLearnedWord))
-            }
+        val splitLine = wordItem.split("|")
+        try {
+            dictionary.add(Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0))
+        }catch (e:Exception){
+            throw RuntimeException("Слово или оригинал пустые.")
         }
     }
     println(dictionary.joinToString(separator = "\n"))
 
 }
 
-fun validateDictionaryString(listWord: List<String>): Boolean {
-    return try {
-        listWord[0].isNotEmpty() && listWord[1].isNotEmpty()
-    } catch (e: Exception) {
-        false
-    }
-}
-
 data class Word(
     val original: String,
     val translated: String,
-    val numberLearnedWord: Int,
+    val correctAnswerCount: Int,
 )
