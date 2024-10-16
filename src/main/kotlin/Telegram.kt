aff6cd197ab00ed1dacae56a1aa8e5cb9aea219e
Иван <cvb@sdf>
2024-10-16 21:39:56 +0300

    KTB-12 Сделал обработку запросов TGApi:GetMe, GetUpdates

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -5,11 +5,9 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-const val TG_TOKEN_ENV_KEY_NAME = "tgToken"
+fun main(args: Array<String>) {
 
-fun main() {
-
-    val botToken = System.getenv()[TG_TOKEN_ENV_KEY_NAME]
+    val botToken = args[0]
     val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
     val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
     val client = HttpClient.newBuilder().build()
