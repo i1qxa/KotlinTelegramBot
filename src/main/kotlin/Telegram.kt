75c84a37b16218b07c475078b14b53f94bec8947
Иван <cvb@sdf>
2024-10-19 23:18:03 +0300

    KTB-15 Внес необходимые правки

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,14 +1,45 @@
 package org.example
 
+import java.net.http.HttpClient
+
+private val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
+private val updateIdRegex = "\"update_id\":(.+?),".toRegex()
+private val chatIdRegex = "\"chat\":\\{\"id\":(.+?),".toRegex()
+
 fun main(args: Array<String>) {
 
+    val client = HttpClient.newBuilder().build()
     val botToken = args[0]
-    val tgBotService = TelegramBotService(botToken)
+    val tgBotService = TelegramBotService(botToken, client)
+    var updateId = 0
     while (true) {
         Thread.sleep(2000)
-        val msg = tgBotService.getMsgText()
+        val updates = tgBotService.getUpdates(updateId)
+        getUpdateId(updates)?.let {
+            updateId = it + 1
+        }
+        val msg = getMsgText(updates)
         println(msg)
-        if (msg == "Hello") tgBotService.sendMessage("Hello")
+        if (msg == "Hello") {
+            getChatId(updates)?.let {
+                tgBotService.sendMessage(it, "Hello")
+            }
+        }
     }
 
+}
+
+fun getMsgText(updates: String): String? {
+    val matchResult = messageTextRegex.find(updates)
+    return matchResult?.groups?.get(1)?.value
+}
+
+fun getUpdateId(updates: String): Int? {
+    val matchResult = updateIdRegex.find(updates)
+    return matchResult?.groups?.get(1)?.value?.toIntOrNull()
+}
+
+fun getChatId(updates: String): Int? {
+    val matchResult = chatIdRegex.find(updates)
+    return matchResult?.groups?.get(1)?.value?.toIntOrNull()
 }
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -5,50 +5,20 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-class TelegramBotService(private val token: String) {
+private const val BASE_URL = "https://api.telegram.org/bot"
 
-    private val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
-    private val updateIdRegex = "\"update_id\":(.+?),".toRegex()
-    private val chatIdRegex = "\"chat\":\\{\"id\":(.+?),".toRegex()
+class TelegramBotService(private val token: String, private val client: HttpClient) {
 
-    private var updates: String = ""
-    private var updateId = 0
-    private var chatId = 0
-
-    fun getMsgText(): String? {
-        getUpdates()
-        val matchResult = messageTextRegex.find(updates)
-        return matchResult?.groups?.get(1)?.value
-    }
-
-    fun sendMessage(text: String) {
-        val urlSendMessage = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatId&text=$text"
-        val client = HttpClient.newBuilder().build()
+    fun sendMessage(chatId: Int, text: String) {
+        val urlSendMessage = "$BASE_URL$token/sendMessage?chat_id=$chatId&text=$text"
         val requestSendMessage = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(requestSendMessage, HttpResponse.BodyHandlers.ofString()).body()
     }
 
-    private fun getUpdates() {
-        val urlGetUpdates = "https://api.telegram.org/bot$token/getUpdates?offset=$updateId"
-        val client = HttpClient.newBuilder().build()
+    fun getUpdates(updateId: Int): String {
+        val urlGetUpdates = "$BASE_URL$token/getUpdates?offset=$updateId"
         val requestGetUpdates = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-        updates = client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString()).body()
-        fetchUpdateId()
-        fetchChatId()
-    }
-
-    private fun fetchUpdateId() {
-        val matchResult = updateIdRegex.find(updates)
-        matchResult?.groups?.get(1)?.value?.toIntOrNull()?.let {
-            updateId = it + 1
-        }
-    }
-
-    private fun fetchChatId() {
-        val matchResult = chatIdRegex.find(updates)
-        matchResult?.groups?.get(1)?.value?.toIntOrNull()?.let {
-            chatId = it
-        }
+        return client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString()).body()
     }
 
 
