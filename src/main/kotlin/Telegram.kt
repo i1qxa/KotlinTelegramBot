bfd579225eaeff2242e8ded591d0ba55985a12a2
Кукса <kuiv1989@gmail.com>
2024-10-22 15:26:46 +0300

    KTB-16 Создал отображение и обработку нажатий кнопок в боте

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,17 +1,16 @@
 package org.example
 
-import java.net.http.HttpClient
-
 private val messageTextRegex = "\"text\":\"(.+?)\"".toRegex()
 private val updateIdRegex = "\"update_id\":(.+?),".toRegex()
 private val chatIdRegex = "\"chat\":\\{\"id\":(.+?),".toRegex()
+private val dataRegex = "\"callback_data\":\"(.+?)\"".toRegex()
 
 fun main(args: Array<String>) {
 
-    val client = HttpClient.newBuilder().build()
     val botToken = args[0]
-    val tgBotService = TelegramBotService(botToken, client)
+    val tgBotService = TelegramBotService(botToken)
     var updateId = 0
+    val trainer = LearnWordsTrainer()
     while (true) {
         Thread.sleep(2000)
         val updates = tgBotService.getUpdates(updateId)
@@ -23,6 +22,16 @@ fun main(args: Array<String>) {
         if (msg == "Hello") {
             getChatId(updates)?.let {
                 tgBotService.sendMessage(it, "Hello")
+            }
+        }
+        if (msg == "start") {
+            getChatId(updates)?.let {
+                tgBotService.sendMenu(it)
+            }
+        }
+        if (getData(updates)) {
+            getChatId(updates)?.let {
+                tgBotService.sendMessage(it, "Выучено 10 из 10 слов 100%")
             }
         }
     }
@@ -42,4 +51,11 @@ fun getUpdateId(updates: String): Int? {
 fun getChatId(updates: String): Int? {
     val matchResult = chatIdRegex.find(updates)
     return matchResult?.groups?.get(1)?.value?.toIntOrNull()
+}
+
+fun getData(updates: String): Boolean {
+    val matchResult = dataRegex.find(updates)
+    println(updates)
+    return matchResult?.groups?.get(1)?.value != null
+
 }
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -1,16 +1,24 @@
 package org.example
 
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
 
 private const val BASE_URL = "https://api.telegram.org/bot"
 
-class TelegramBotService(private val token: String, private val client: HttpClient) {
+class TelegramBotService(private val token: String) {
+
+    private val client = HttpClient.newBuilder().build()
 
     fun sendMessage(chatId: Int, text: String) {
-        val urlSendMessage = "$BASE_URL$token/sendMessage?chat_id=$chatId&text=$text"
+        val encoded = URLEncoder.encode(
+            text,
+            StandardCharsets.UTF_8
+        )
+        val urlSendMessage = "$BASE_URL$token/sendMessage?chat_id=$chatId&text=$encoded"
         val requestSendMessage = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(requestSendMessage, HttpResponse.BodyHandlers.ofString()).body()
     }
@@ -21,5 +29,33 @@ class TelegramBotService(private val token: String, private val client: HttpClie
         return client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString()).body()
     }
 
+    fun sendMenu(chatId: Int) {
+        val urlSendMenu = "$BASE_URL$token/sendMessage"
+        val sendMenuBody = """
+            {
+                "chat_id":$chatId,
+                "text":"Основное меню",
+                "reply_markup":{
+                    "inline_keyboard":[
+                        [
+                            {    
+                            "text":"Учить слова",
+                            "callback_data":"learn_words_clicked"
+                            },
+                            {
+                            "text":"Статистика",
+                            "callback_data":"statistics_clicked"
+                            }
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+        val requestSendMenu = HttpRequest.newBuilder().uri(URI.create(urlSendMenu))
+            .header("Content-type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody))
+            .build()
+        client.send(requestSendMenu, HttpResponse.BodyHandlers.ofString()).body()
+    }
 
 }
